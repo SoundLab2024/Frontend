4bb6de4b5381691de34fda0603c4b17c7f6e902b
saltyDario <94072049+saltyDario@users.noreply.github.com>
2023-12-01 18:08:47 +0100

    Cambiamenti Minori
    Fixato Manifest e aggiunto Tema della Splash Screen

---
+++ SoundLab/app/src/main/java/view/activity/SlideShow.java
@@ -0,0 +1,16 @@
+package view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.soundlab.R;
+
+public class SlideShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+    }
+}
